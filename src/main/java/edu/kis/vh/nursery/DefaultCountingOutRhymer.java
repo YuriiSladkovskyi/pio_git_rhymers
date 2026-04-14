82a635b0db8a118085b941e261f40bd92ec36184
YuriiSladkovskyi <258498@edu.p.lodz.pl>
2026-04-15 00:20:41 +0200

    deklaracje stałych SIZE i IF_EMPTY

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int SIZE = 12;
+    public static final int IF_EMPTY = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[SIZE];
+    public int total = IF_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
