f03f54ddc57e6217bb2124396a7a14b90b3c4e9e
YuriiSladkovskyi <258498@edu.p.lodz.p>
2026-04-15 01:15:14 +0200

    9.1 Zmiana na najmniejsze możliwe modyfikatory widocznosni atrybutow klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = 12;
-    public static final int IF_EMPTY = -1;
+    private static final int SIZE = 12;
+    private static final int IF_EMPTY = -1;
     private static final int DEFAULT_VALUE = -1;
 
     private final int[] numbers = new int[SIZE];
