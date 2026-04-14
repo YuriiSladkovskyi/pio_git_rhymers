1c8292ddc933b29a4ec7614eabc6b607db8b3588
YuriiSladkovskyi <258498@edu.p.lodz.p>
2026-04-15 01:18:01 +0200

    10. Stworzenie getter dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,6 +9,10 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[SIZE];
     public int total = IF_EMPTY;
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
